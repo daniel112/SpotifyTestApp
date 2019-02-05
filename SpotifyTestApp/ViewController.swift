f2b497335f802f9bde9a8da11a91aad07e82d8b1
Daniel Yo <dnl992004@yahoo.com.au>
2019-02-05 09:58:39 -0700

    Initial Commit

---
+++ SpotifyTestApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SpotifyTestApp
+//
+//  Created by Daniel Yo on 2/5/19.
+//  Copyright © 2019 Daniel Yo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
